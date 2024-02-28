c577677474be40c818580f5e8b24b22cc1c2e4f3
Vancoillie <jeremy.vancoillie@gmail.com>
2024-02-28 18:52:36 +0100

    Cup23 Points fixes

---
+++ php/src/Challenge/Cup23/PointsFixes/ChallengeResolver.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Challenge\Cup23\PointsFixes;
+
+use App\Challenge\AbstractChallengeResolver;
+
+/**
+ * Class ChallengeResolver.
+ *
+ * @see https://pydefis.callicode.fr/defis/C23_PointsFixes/txt
+ */
+class ChallengeResolver extends AbstractChallengeResolver
+{
+    public function main(): string
+    {
+        $rounds = [0 => 0, 1 => 0];
+        $i = 0;
+        foreach (range(2000, 9999) as $year) {
+            $split = array_map('intval', str_split((string) $year));
+            $s = array_sum(array_map(fn (int $n) => ($n > 4) ? $n - 4 : $n, $split)) + count($split) + 1;
+
+            if (0 === $year % $s) {
+                ++$rounds[$i];
+            }
+
+            if (2999 === $year) {
+                $i = 1;
+            }
+        }
+
+        $rounds[1] += $rounds[0];
+
+        return (string) json_encode($rounds);
+    }
+}
